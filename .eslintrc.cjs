c3845d2cf68d88f561478dfefbbcbd298a97a714
vasquez-esteban <evasquezp@unal.edu.co>
2024-06-10 11:00:25 -0500

    Added eslintignore & jsx-a11y. Fixed Accessibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:astro/recommended",
+    "plugin:jsx-a11y/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
